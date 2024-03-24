e548dd80bd0b2c491dab6580dfbdd9f57ddef133
muammar200 <amuammar01@gmail.com>
2024-03-24 22:36:44 +0800

    feat: Create 'Booking' model
        This commit creates the 'Booking' model with the following features:
        - It belongs to an 'Item' and a 'User'.
        - It uses SoftDeletes for soft deletion.
        - It has guarded 'id' field.
        - It has dates 'start_date' and 'end_date'.

---
+++ app/Models/Booking.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Booking extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'start_date',
+        'end_date',
+    ];
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function user(): BelongsTo
+    {
+       return $this->belongsTo(User::class);
+    }
+}
