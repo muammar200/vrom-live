a5066e2d7cc4c2618da9144cc3cd8a240bf0e69e
muammar200 <amuammar01@gmail.com>
2024-03-24 22:44:00 +0800

    modify: Update 'User' model
      - Change $fillable property to $guarded.
      - Add 'bookings' relationship using the HasMany relationship.
      - Remove 'name', 'email', and 'password' fields from the $fillable array (not needed as they are now guarded).

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -23,12 +24,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
 
+    protected $guarded = ['id'];
+    
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -58,4 +61,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function bookings() : HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
