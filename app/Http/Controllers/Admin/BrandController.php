3c1f6cb23463e09e919c08c6bcce4cc908311a0c
muammar200 <amuammar01@gmail.com>
2024-04-05 23:16:16 +0800

    feat: Added CRUD functionality for brands, and also added BrandRequest for brand validation
    This commit adds the BrandController with methods for CRUD operations on brands. Key features include:
    
    - Index method for displaying a list of brands using Yajra DataTables with AJAX support.
    - Create method for displaying the brand creation form.
    - Store method for validating and storing a new brand in the database.
    - Edit method for displaying the brand edit form.
    - Update method for validating and updating an existing brand in the database.
    - Destroy method for deleting a brand.
    - The BrandRequest class is used for form validation, and the 'slug' attribute is generated using the Str::slug and Str::random methods.

---
+++ app/Http/Controllers/Admin/BrandController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Brand;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BrandRequest;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Script DataTables, AJAX
+        if(request()->ajax()){
+            $query = Brand::query();
+
+            return DataTables::of($query)
+            ->addColumn('action', function ($brand){
+                return '
+                    <a class="block px-2 py-1 mb-1 text-xs text-center text-white transition duration-500 bg-gray-700 border border-gray-700 rounded-md select-none ease hover:bg-gray-800 focus:outline-none focus:shadow-outline" href="'.route('admin.brands.edit', $brand->id).'">
+                    Sunting
+                    </a>
+                    <form class="block w-full" onsubmit="return confirm(\'Apakah anda yakin?\');" -block" action="' . route('admin.brands.destroy', $brand->id) . '" method="POST">
+                <button class="w-full px-2 py-1 text-xs text-white transition duration-500 bg-red-500 border border-red-500 rounded-md select-none ease hover:bg-red-600 focus:outline-none focus:shadow-outline">
+                    Hapus
+                </button>          
+                        ' . method_field('delete') . csrf_field() . '
+                    </form>';
+            })
+            ->rawColumns(['action'])
+            ->make(); 
+        }
+
+        // Script return page view brand
+        return view( 'admin.brands.index' );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.brands.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(BrandRequest $request)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']) . '-' . Str::lower(Str::random(5));
+
+        Brand::create($data);
+
+        return redirect()->route('admin.brands.index');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Brand $brand)
+    {
+        return view('admin.brands.edit',[
+            'brand' => $brand,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(BrandRequest $request, Brand $brand)
+    {
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']) . '-' . Str::lower(Str::random(5));
+
+        $brand->update($data);
+
+        return redirect()->route('admin.brands.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Brand $brand)
+    {
+        $brand->delete();
+
+        return redirect()->route('admin.brands.index');
+    }
+}
