398a00ff6aed798c47220fc15bb225bb1fe6c2f4
muammar200 <amuammar01@gmail.com>
2024-04-05 23:51:49 +0800

    feat: Added routes for admin dashboard and CRUD operations
    This commit adds routes for the admin dashboard and CRUD operations for brands, types, items, and bookings.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TesController;
+use App\Http\Controllers\Admin\BookingController as AdminBookingController;
+use App\Http\Controllers\Admin\ItemController as AdminItemController;
+use App\Http\Controllers\Admin\TypeController as AdminTypeController;
+use App\Http\Controllers\Admin\BrandController as AdminBrandController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +22,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
+Route::prefix('admin')->name('admin.')->middleware([
+'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    'admin'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+        
+        Route::resource('brands', AdminBrandController::class);
+        Route::resource('types', AdminTypeController::class);    
+        Route::resource('items', AdminItemController::class);    
+        Route::resource('bookings', AdminBookingController::class);    
 });
+
