378399ffcaac8615db58020756f1bc4e242662c4
muammar200 <amuammar01@gmail.com>
2024-04-05 23:34:51 +0800

    feat: Added BookingController with CRUD operations
    This commit introduces the BookingController, which provides CRUD operations for managing bookings. Key features include:
    
    - Index method for displaying a list of bookings using Yajra DataTables with AJAX support.
    - Edit method for displaying the booking edit form.
    - Update method for validating and updating an existing booking in the database.
    - Destroy method for deleting a booking from the database.
    
    The BookingRequest class is used for form validation.

---
+++ app/Http/Requests/BookingRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // 'item_id' => ['required', 'integer', 'exists:items,id'], 
+            // 'user_id' => ['required', 'integer', 'exists:users,id'],
+            'name' => ['nullable', 'string', 'max:255'], 
+            // 'start_date' => ['required', 'date', 'after:today'],
+            // 'end_date' => ['required', 'date', 'after:start_date'], 
+            'address' => ['nullable', 'string'],
+            'city' => ['nullable', 'string', 'max:255'],
+            'zip' => ['nullable', 'string', 'max:255'], 
+            'status' => ['required', 'string', 'in:pending,confirmed,done,cancelled'],
+            // 'payment_method' => ['required', 'string', 'in:midtrans,other'], 
+            'payment_status' => ['required', 'string', 'in:pending,success,failed,expired'],
+            // 'payment_url' => ['nullable', 'string', 'url'], 
+            // 'total_price' => ['required', 'numeric', 'min:0.01'],
+        ];
+    }
+}
